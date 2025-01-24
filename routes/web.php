c4b8ea954f24820a24b838e01c18fa4699c66470
Luciano Gomes <lgomesroc2012@gmail.com>
2025-01-24 17:36:30 -0300

    Teste de PHP POO incluindo tipagem e sem banco de dados

--- routes/web.php
+++ routes/web.php
@@ -3,4 +3,4 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CartController;
 
-Route::get('/add-items', [CartController::class, 'handRequest']);
+Route::get('/add-items', [CartController::class, 'handLeRequest']);
